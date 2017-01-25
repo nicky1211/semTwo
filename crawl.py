6847ca2a9e9bf7f416da2048dbd55d7c0e32730e
Manick Mehra <manickmehra2006@yahoo.com>
2017-01-25 18:49:29 +0530

    With the help of os and subprocess module and curl command we now save the data into specific folders

--- crawl.py
+++ crawl.py
@@ -1,10 +1,14 @@
 import requests
 import re
+import os
+import subprocess
+
 from bs4 import BeautifulSoup
 source_code = requests.get('https://en.wikipedia.org/wiki/Category:Diseases_and_disorders')
 plain_text = source_code.text
 soup = BeautifulSoup(plain_text,"lxml")
 linkList = []
+docBasePath = '/home/manick/projectDocuments'
 
 def getaTags(soup):
     for link in soup.findAll('a'):
@@ -20,5 +24,16 @@ def getDiseaseCategoryLinks(aTags):
         linkDict[cateGory] = href
     return linkDict
 
+def deepCrawl(path,cat,url):
+
+	path = path+"/"+cat
+	fileName = path+"/"+cat+".html"
+	if not os.path.exists(path):
+		os.makedirs(path)
+		subprocess.call(["curl", "-#" ,"-o", "%s"%fileName, "%s"%url])
 aTags = getaTags(soup) 
 links = getDiseaseCategoryLinks(aTags)
+
+for key, value in links.iteritems():
+	print key,value
+	deepCrawl('/home/manick/proData',key,value)
