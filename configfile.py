7a6122d9e19c72e39493ed8376a7eae8f12f1217
Manick Mehra <manickmehra2006@yahoo.com>
2017-01-25 19:04:46 +0530

    1: Added a config file 2: Changed the code to move files into specific folders

---
+++ configfile.py
@@ -0,0 +1,3 @@
+basecrawlurl="https://en.wikipedia.org/wiki/Category:Diseases_and_disorders"
+docBasePath = '/home/manick/wikiDocuments'
+website = "https://en.wikipedia.org"
--- crawl.py
+++ crawl.py
@@ -2,25 +2,27 @@ import requests
 import re
 import os
 import subprocess
-
+import configfile
 from bs4 import BeautifulSoup
-source_code = requests.get('https://en.wikipedia.org/wiki/Category:Diseases_and_disorders')
+
+
+source_code = requests.get(configfile.basecrawlurl)
 plain_text = source_code.text
 soup = BeautifulSoup(plain_text,"lxml")
 linkList = []
-docBasePath = '/home/manick/projectDocuments'
+
 
 def getaTags(soup):
     for link in soup.findAll('a'):
         l =  link.get('href')
-        linkList.append(l)
+        linkList.append(l)	
     return linkList
 
 def getDiseaseCategoryLinks(aTags):
     linkDict = {}
     for href in aTags[44:77]:
     	cateGory = re.findall(r'/(\w+)', href)[1]
-        href = "https://en.wikipedia.org"+ href
+        href = configfile.website+href
         linkDict[cateGory] = href
     return linkDict
 
@@ -30,10 +32,12 @@ def deepCrawl(path,cat,url):
 	fileName = path+"/"+cat+".html"
 	if not os.path.exists(path):
 		os.makedirs(path)
-		subprocess.call(["curl", "-#" ,"-o", "%s"%fileName, "%s"%url])
+		subprocess.call(["curl", "-s" ,"-o", "%s"%fileName, "%s"%url])
+
+
 aTags = getaTags(soup) 
 links = getDiseaseCategoryLinks(aTags)
 
 for key, value in links.iteritems():
-	print key,value
-	deepCrawl('/home/manick/proData',key,value)
+	# print key,value
+	deepCrawl(configfile.docBasePath,key,value)
