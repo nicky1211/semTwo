45cefab303cac10171d41d5c248d1a89bef07a85
Manick Mehra <manickmehra2006@yahoo.com>
2017-01-25 16:38:27 +0530

    Changed the code to get only the specific links related to the pages in category Diseases and disorders

--- crawl.py
+++ crawl.py
@@ -1,59 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
+source_code = requests.get('https://en.wikipedia.org/wiki/Category:Diseases_and_disorders')
+plain_text = source_code.text
+soup = BeautifulSoup(plain_text,"lxml")
+linkList = []
 
-''' The spider code to retrieve the first page on wiki, using these links as the starting point navigation
-	to other pages will happen in the similar manner'''
+def getaTags(soup):
+    for link in soup.findAll('a'):
+        l =  link.get('href')
+        linkList.append(l)
+    return linkList
 
-class Spider:
+def getDiseaseCategoryLinks(aTags):
+    hrefList = []
+    for each in aTags[43:77]:
+        each = "https://en.wikipedia.org"+each
+        hrefList.append(each)
+    return hrefList
 
-	# As the constructor class, we just get the page source in plain text format
-	def __init__(self,url):
-		self.source_code = requests.get(url)
-		self.plain_text = self.source_code.text
-
-	# SubCategory : The plain source code text is now used to obtain the specific list of url which have the 'class':'CategoryTreeLabel'
-	def subcategory_spider(self):
-		self.subcategory_crawl_list = []
-		soup = BeautifulSoup(self.plain_text,"lxml")
-		for link in soup.findAll('a',{'class':'CategoryTreeLabel'}):
-			href = link.get('href')
-			href = "https://en.wikipedia.org"+href
-			self.subcategory_crawl_list.append(href)
-		assert(len(self.subcategory_crawl_list) == 32)
-		return self.subcategory_crawl_list
-
-	# Category : The plain source code text is now used to obtain the specific list of url which have the 'class' : 'mw-category-group'
-	def category_spider(self):
-		self.category_crawl_list = []
-		soup = BeautifulSoup(self.plain_text,"lxml")
-		cat_class = soup.findAll('div', attrs={'class' : 'mw-category-group'})
-		for each in cat_class:
-			href = each.find('a')['href']
-			href = "https://en.wikipedia.org"+href
-			self.category_crawl_list.append(href)
-		assert(len(self.category_crawl_list) == 33)
-		return self.category_crawl_list
-
-class CrawlL1:
-
-	def maincrawler(self,url):
-		source_code = requests.get(url)
-		plain_text = source_code.text
-		soup = BeautifulSoup(plain_text,"lxml")
-		for link in soup.findAll('a',{'class':'CategoryTreeLabel  CategoryTreeLabelNs14 CategoryTreeLabelCategory'}):
-			href = link.get('href')
-			href = "https://en.wikipedia.org"+href
-			print href
-
-
-
-
-if __name__ == '__main__':
-
-	url = 'https://en.wikipedia.org/wiki/Category:Diseases_and_disorders'
-	spider = Spider(url)
-	list1 = spider.subcategory_spider()
-	list2 = spider.category_spider()
-	crawl = CrawlL1()
-	for each in list1:
-		crawl.maincrawler(each)
+aTags = getaTags(soup) 
+links = getDiseaseCategoryLinks(aTags)
+print links
